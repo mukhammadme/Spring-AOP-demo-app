ed697850962df0da6dc93dfacaae9b9f90f208a4
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-22 22:05:06 +0500

    Spring Tutorial 26 - Writing Our First Aspect Part 1

---
+++ src/com/linc/AopMain.java
@@ -0,0 +1,13 @@
+package com.linc;
+
+import com.linc.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+        System.out.println("Circle: " + shapeService.getCircle().getName());
+    }
+}
