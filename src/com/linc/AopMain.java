b98c10ccdb471f60a1fa8320f3ce5a1ab4f542c9
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-24 22:44:50 +0500

    Spring Tutorial 29 - A Few More Pointcut Expressions

--- src/com/linc/AopMain.java
+++ src/com/linc/AopMain.java
@@ -8,6 +8,6 @@ public class AopMain {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
-        System.out.println("Circle: " + shapeService.getTriangle().getName());
+        System.out.println("Circle: " + shapeService.getCircle().getName());
     }
 }
--- src/com/linc/aspect/LoggingAspect.java
+++ src/com/linc/aspect/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-    @Before("allGetters()")
+    @Before("allGetters() && allCircleMethods()")
     public void LoggingAdvice() {
         System.out.println("Advice run. Get method called");
     }
@@ -19,4 +19,7 @@ public class LoggingAspect {
 
     @Pointcut("execution(* get*(..))")
     public void allGetters() {}
+
+    @Pointcut("within(com.linc.model.Circle)")
+    public void allCircleMethods() {}
 }
