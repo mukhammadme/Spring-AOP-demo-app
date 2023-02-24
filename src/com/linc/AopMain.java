bafb479e184593513eab46b6cf559e2c7d52c1ac
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-24 23:23:53 +0500

    Spring Tutorial 30 - JoinPoints and Advice Arguments

--- src/com/linc/AopMain.java
+++ src/com/linc/AopMain.java
@@ -8,6 +8,7 @@ public class AopMain {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+        shapeService.getCircle().setName("New Name");
         System.out.println("Circle: " + shapeService.getCircle().getName());
     }
 }
--- src/com/linc/aspect/LoggingAspect.java
+++ src/com/linc/aspect/LoggingAspect.java
@@ -1,20 +1,21 @@
 package com.linc.aspect;
 
-
+import com.linc.model.Circle;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-    @Before("allGetters() && allCircleMethods()")
-    public void LoggingAdvice() {
-        System.out.println("Advice run. Get method called");
+    @Before("allCircleMethods()")
+    public void LoggingAdvice(JoinPoint joinPoint) {
+        Circle circle = (Circle) joinPoint.getTarget();
     }
 
-    @Before("allGetters()")
-    public void secondAdvice() {
-        System.out.println("Second advice executed");
+    @Before("args(name)")
+    public void stringArgMethods(String name) {
+        System.out.println("A method that takes String Arguments called. The value is: " + name);
     }
 
     @Pointcut("execution(* get*(..))")
